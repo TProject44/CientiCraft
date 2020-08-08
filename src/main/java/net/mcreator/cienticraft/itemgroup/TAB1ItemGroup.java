5efb669cfb8ebe54305ed7b6025e4567c4c08c65
TProject44 <ADMIN@ADMIN-PC>
2020-08-08 16:02:24 -0300

    adicionadas as ceringas com dna

---
+++ src/main/java/net/mcreator/cienticraft/itemgroup/TAB1ItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.cienticraft.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.cienticraft.item.CeringaorcoItem;
+import net.mcreator.cienticraft.CienticraftModElements;
+
+@CienticraftModElements.ModElement.Tag
+public class TAB1ItemGroup extends CienticraftModElements.ModElement {
+	public TAB1ItemGroup(CienticraftModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabtab_1") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(CeringaorcoItem.block, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}

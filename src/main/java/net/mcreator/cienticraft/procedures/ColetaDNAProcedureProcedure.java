04f0c658f658a9cddc294c763c39291ad1871953
TProject44 <andrey.zeferino@outlook>
2020-08-09 16:13:12 -0300

    novas ceringas

--- src/main/java/net/mcreator/cienticraft/procedures/ColetaDNAProcedureProcedure.java
+++ src/main/java/net/mcreator/cienticraft/procedures/ColetaDNAProcedureProcedure.java
@@ -1,11 +1,45 @@
 package net.mcreator.cienticraft.procedures;
 
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.passive.PandaEntity;
+import net.minecraft.entity.passive.MooshroomEntity;
+import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.monster.SpiderEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.cienticraft.item.CeringazombiItem;
+import net.mcreator.cienticraft.item.CeringavacacogumeloItem;
+import net.mcreator.cienticraft.item.CeringavacaItem;
+import net.mcreator.cienticraft.item.CeringaporcoItem;
+import net.mcreator.cienticraft.item.CeringapandaItem;
+import net.mcreator.cienticraft.item.CeringaovelhaItem;
+import net.mcreator.cienticraft.item.CeringagalinhaItem;
+import net.mcreator.cienticraft.item.CeringaaranhaItem;
+import net.mcreator.cienticraft.item.CeringaItem;
+import net.mcreator.cienticraft.item.CeringaFoxItem;
+import net.mcreator.cienticraft.CienticraftModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
 @CienticraftModElements.ModElement.Tag
 public class ColetaDNAProcedureProcedure extends CienticraftModElements.ModElement {
-
 	public ColetaDNAProcedureProcedure(CienticraftModElements instance) {
 		super(instance, 10);
-
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
@@ -18,10 +52,8 @@ public class ColetaDNAProcedureProcedure extends CienticraftModElements.ModEleme
 			System.err.println("Failed to load dependency sourceentity for procedure ColetaDNAProcedure!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
 		Entity sourceentity = (Entity) dependencies.get("sourceentity");
-
 		if (((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
 				.getItem() == new ItemStack(CeringaItem.block, (int) (1)).getItem()) && (entity instanceof MooshroomEntity))) {
 			if (sourceentity instanceof PlayerEntity)
@@ -106,17 +138,56 @@ public class ColetaDNAProcedureProcedure extends CienticraftModElements.ModEleme
 			}
 			entity.attackEntityFrom(DamageSource.GENERIC, (float) 2);
 		}
-
+		if (((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(CeringaItem.block, (int) (1)).getItem()) && (entity instanceof ZombieEntity))) {
+			if (sourceentity instanceof PlayerEntity)
+				((PlayerEntity) sourceentity).inventory.clearMatchingItems(
+						p -> ((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == p.getItem(),
+						(int) 1);
+			if (sourceentity instanceof PlayerEntity) {
+				ItemStack _setstack = new ItemStack(CeringazombiItem.block, (int) (1));
+				_setstack.setCount((int) 1);
+				ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) sourceentity), _setstack);
+			}
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 2);
+		}
+		if (((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(CeringaItem.block, (int) (1)).getItem()) && (entity instanceof PandaEntity))) {
+			if (sourceentity instanceof PlayerEntity)
+				((PlayerEntity) sourceentity).inventory.clearMatchingItems(
+						p -> ((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == p.getItem(),
+						(int) 1);
+			if (sourceentity instanceof PlayerEntity) {
+				ItemStack _setstack = new ItemStack(CeringapandaItem.block, (int) (1));
+				_setstack.setCount((int) 1);
+				ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) sourceentity), _setstack);
+			}
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 2);
+		}
+		if (((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(CeringaItem.block, (int) (1)).getItem()) && (entity instanceof FoxEntity))) {
+			if (sourceentity instanceof PlayerEntity)
+				((PlayerEntity) sourceentity).inventory.clearMatchingItems(
+						p -> ((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == p.getItem(),
+						(int) 1);
+			if (sourceentity instanceof PlayerEntity) {
+				ItemStack _setstack = new ItemStack(CeringaFoxItem.block, (int) (1));
+				_setstack.setCount((int) 1);
+				ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) sourceentity), _setstack);
+			}
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 2);
+		}
 	}
 
 	@SubscribeEvent
 	public void onRightClickEntity(PlayerInteractEvent.EntityInteract event) {
 		Entity entity = event.getTarget();
 		PlayerEntity sourceentity = event.getPlayer();
-
 		if (event.getHand() != sourceentity.getActiveHand())
 			return;
-
 		int i = event.getPos().getX();
 		int j = event.getPos().getY();
 		int k = event.getPos().getZ();
@@ -131,5 +202,4 @@ public class ColetaDNAProcedureProcedure extends CienticraftModElements.ModEleme
 		dependencies.put("event", event);
 		this.executeProcedure(dependencies);
 	}
-
 }

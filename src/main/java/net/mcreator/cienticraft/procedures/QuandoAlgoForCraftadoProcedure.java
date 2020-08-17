7dedf9427b759acb62be2954bbc563e73a9fde7b
TProject44 <Andrey@DESKTOP-KCL21OP.local>
2020-08-17 18:35:33 -0300

    corrigindo um bugzin

--- src/main/java/net/mcreator/cienticraft/procedures/QuandoAlgoForCraftadoProcedure.java
+++ src/main/java/net/mcreator/cienticraft/procedures/QuandoAlgoForCraftadoProcedure.java
@@ -1,15 +1,67 @@
 package net.mcreator.cienticraft.procedures;
 
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.cienticraft.item.CeringacontaminadaItem;
 import net.mcreator.cienticraft.CienticraftModElements;
 
 import java.util.Map;
+import java.util.HashMap;
 
 @CienticraftModElements.ModElement.Tag
 public class QuandoAlgoForCraftadoProcedure extends CienticraftModElements.ModElement {
 	public QuandoAlgoForCraftadoProcedure(CienticraftModElements instance) {
-		super(instance, 36);
+		super(instance, 53);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure QuandoAlgoForCraftado!");
+			return;
+		}
+		if (dependencies.get("itemstack") == null) {
+			System.err.println("Failed to load dependency itemstack for procedure QuandoAlgoForCraftado!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
+		if ((ItemTags.getCollection().getOrCreate(new ResourceLocation(("tubo_dna_mob").toLowerCase(java.util.Locale.ENGLISH)))
+				.contains((itemstack).getItem()))) {
+			if (entity instanceof PlayerEntity) {
+				ItemStack _setstack = new ItemStack(CeringacontaminadaItem.block, (int) (1));
+				_setstack.setCount((int) 1);
+				ItemHandlerHelper.giveItemToPlayer(((PlayerEntity) entity), _setstack);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onItemCrafted(PlayerEvent.ItemCraftedEvent event) {
+		Entity entity = event.getPlayer();
+		World world = entity.world;
+		double i = entity.getPosX();
+		double j = entity.getPosY();
+		double k = entity.getPosZ();
+		ItemStack itemStack = event.getCrafting();
+		Map<String, Object> dependencies = new HashMap<>();
+		dependencies.put("x", i);
+		dependencies.put("y", j);
+		dependencies.put("z", k);
+		dependencies.put("world", world);
+		dependencies.put("entity", entity);
+		dependencies.put("itemstack", itemStack);
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
 	}
 }

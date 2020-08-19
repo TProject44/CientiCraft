3379326d0714b2e78e1f6b03e48dd19de828e084
TProject44 <ADMIN@192.168.0.104>
2020-08-19 16:12:10 -0300

    adicionadas novas ceringas TestC10

--- src/main/java/net/mcreator/cienticraft/itemgroup/TAB1ItemGroup.java
+++ src/main/java/net/mcreator/cienticraft/itemgroup/TAB1ItemGroup.java
@@ -7,7 +7,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 
-import net.mcreator.cienticraft.item.CeringaporcoItem;
+import net.mcreator.cienticraft.item.Tab01Item;
 import net.mcreator.cienticraft.CienticraftModElements;
 
 @CienticraftModElements.ModElement.Tag
@@ -22,7 +22,7 @@ public class TAB1ItemGroup extends CienticraftModElements.ModElement {
 			@OnlyIn(Dist.CLIENT)
 			@Override
 			public ItemStack createIcon() {
-				return new ItemStack(CeringaporcoItem.block, (int) (1));
+				return new ItemStack(Tab01Item.block, (int) (1));
 			}
 
 			@OnlyIn(Dist.CLIENT)
